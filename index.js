9e482cc6be29303623c1a860b247a5420c82b189
Mbaduko <nsengiyumvaclement247@gmail.com>
2026-04-30 10:21:00 +0200

    feat: Updating imports to ES6 module

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import {saveProduct} from './src/controllers/productControllers.js'
+import {saveProduct, getProducts, updateProduct} from './src/controllers/productControllers.js'
 import {prisma} from './src/config/prisma.js';
 
 dotenv.config();
@@ -12,45 +12,9 @@ app.use(express.json());
 
 app.post('/products', saveProduct);
 
-app.get('/products', async (req, res, next) => {
-    try {
-        const products = await prisma.product.findMany();
-        return res.status(200).json({
-            success: true,
-            data: products
-        });
-    } catch (error) {
-        console.error("Error getting products:", error);
-        return res.status(500).json({
-            success: false,
-            message: "error fetching products"
-        });
-    }
-});
+app.get('/products', getProducts);
 
-app.put('/products/:id', async (req, res, next) => {
-    try {
-        const id = parseInt(req.params.id);
-        const updates = req.body;
-
-
-        const updatedProduct = await prisma.product.update({
-            where: { id },
-            data: updates
-        });
-
-        return res.status(200).json({
-            success: true,
-            data: updatedProduct
-        });
-    } catch (error) {
-        console.error("Error updating product;", error);
-        return res.status(500).json({
-            success: false,
-            message: "error updating product"
-        });
-    };
-});
+app.put('/products/:id', updateProduct);
 
 
 app.listen(PORT, async () => {
--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -20,6 +20,46 @@ export const saveProduct = async (req, res, next) => {
     }
 }
 
+export const getProducts = async (req, res, next) => {
+    try {
+        const products = await prisma.product.findMany();
+        return res.status(200).json({
+            success: true,
+            data: products
+        });
+    } catch (error) {
+        console.error("Error getting products:", error);
+        return res.status(500).json({
+            success: false,
+            message: "error fetching products"
+        });
+    }
+}
+
+export const updateProduct = async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        const updates = req.body;
+
+
+        const updatedProduct = await prisma.product.update({
+            where: { id },
+            data: updates
+        });
+
+        return res.status(200).json({
+            success: true,
+            data: updatedProduct
+        });
+    } catch (error) {
+        console.error("Error updating product;", error);
+        return res.status(500).json({
+            success: false,
+            message: "error updating product"
+        });
+    };
+}
+
 // module.exports = {
 //     saveProduct
 // }
